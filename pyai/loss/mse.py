2421a9de26e054227c3ac99a1f1d7c0e26f03cd7
Claudio Scheer <claudioscheer@protonmail.com>
2021-02-26 19:36:26 -0300

    Remove MAE loss function

--- pyai/loss/mse.py
+++ pyai/loss/mse.py
@@ -22,7 +22,11 @@ class MSELoss(BaseLoss):
 
 if __name__ == "__main__":
     criterion = MSELoss()
-    a = np.array([[-0.5855, 0.4962, -0.7684], [0.0587, 0.5546, 0.9823]])
-    b = np.array([[0.7184, -1.3773, 0.9070], [-0.1963, -3.2091, -0.8386]])
+    a = np.array([2.2, 3])
+    b = np.array([1.8, 4])
     loss = criterion(a, b)
+    gradients = criterion.gradients(a, b)
+    gradients_lim = criterion.gradients_lim(a, b)
     print(loss, loss.shape)
+    print(gradients, gradients.shape)
+    print(gradients_lim, gradients_lim.shape)
