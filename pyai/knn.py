436c41ad10ba8b396842b8a61831fb347b031685
Claudio Scheer <claudioscheer@protonmail.com>
2021-05-01 02:55:45 -0300

    Euclidean distance for kNN

--- pyai/knn.py
+++ pyai/knn.py
@@ -1,6 +1,18 @@
-class kNN:
-    def fit(X, y):
-        pass
+from pyai import Tensor
+import numpy as np
 
-    def predict(x):
+
+class kNN:
+    def __init__(self, distance_function=None):
+        self.distance_function = distance_function or self.__euclidean_distance
+
+    def __euclidean_distance(self, x1: Tensor, x2: Tensor) -> float:
+        return np.sqrt(np.sum((x1 - x2) ** 2))
+
+    def fit(self, X: Tensor, y: Tensor) -> None:
+        # TODO: calculate distances.
+        self.X = X
+        self.y = y
+
+    def predict(self, x: Tensor):
         pass
